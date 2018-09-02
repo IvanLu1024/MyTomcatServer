7cb8f104bfb70a71ba4e2891d7e7563d89368c5c
Ivan Lu <32642894+IvanLu1024@users.noreply.github.com>
2018-09-02 17:18:39 +0800

    添加一些注释信息

--- src/web/tomcat/TomcatServer.java
+++ src/web/tomcat/TomcatServer.java
@@ -14,12 +14,16 @@ public class TomcatServer {
 
     public static void main(String[] args) {
         try {
+            /*
+            开启一个端口号为8080，
+            服务器套接字等待通过网络进入的请求。 它根据该请求执行一些操作，然后可能将结果返回给请求者。
+             */
             ServerSocket ss=new ServerSocket(8080);
+            //死循环，不断接受请求
             while (true){
                 Socket socket=ss.accept();
-                //开启一个线程，处理单次请求
+                //开启一个线程，处理一次请求
                 new Thread(new Handler(socket)).start();
-
             }
         }catch (IOException e){
             e.printStackTrace();
